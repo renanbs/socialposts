0ddf26b84b8427049ad29e8929468a0323e88d52
Renan <renan.biegel@gmail.com>
2017-07-24 23:12:31 -0300

    Edit post

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from .views import (
     post_list,
     post_create,
     post_detail,
-    # post_update,
+    post_update,
     post_delete,
 )
 
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^$', post_list, name="list"),
     url(r'^create/$', post_create),
     url(r'^(?P<id>\d+)/$', post_detail, name="detail"),
-    # url(r'^(?P<slug>[\w-]+)/edit/$', post_update, name="update"),
+    url(r'^(?P<id>\d+)/edit/$', post_update, name="update"),
     url(r'^(?P<id>\d+)/delete/$', post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -77,3 +77,21 @@ def post_delete(request, id=None):
     instance.delete()
     # messages.success(request, "Successfully Deleted")
     return redirect("posts:list")
+
+
+def post_update(request, id=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+    instance = get_object_or_404(Post, id=id)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        # messages.success(request, "Saved")
+        return HttpResponseRedirect(instance.get_absolute_url())
+    context = {
+        "title": instance.title,
+        "instance": instance,
+        "form": form,
+    }
+    return render(request, "post_form.html", context)
