2bc23b238d78edd91bdace48d4fdbbfff7613161
renan.biegel <renan.biegel@gmail.com>
2017-07-26 19:36:24 -0300

    group edit

--- groups/urls.py
+++ groups/urls.py
@@ -4,7 +4,7 @@ from .views import (
     group_list,
     group_create,
     group_detail,
-    # group_update,
+    group_update,
     # group_delete,
 )
 
@@ -12,6 +12,6 @@ urlpatterns = [
     url(r'^$', group_list, name="list"),
     url(r'^create/$', group_create),
     url(r'^(?P<id>\d+)/$', group_detail, name="detail"),
-    # url(r'^(?P<id>\d+)/edit/$', group_update, name="update"),
+    url(r'^(?P<id>\d+)/edit/$', group_update, name="update"),
     # url(r'^(?P<id>\d+)/delete/$', group_delete),
 ]
