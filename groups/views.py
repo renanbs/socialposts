043f182de27e537d291eb5cc958ac3f8a7a054aa
renan.biegel <renan.biegel@gmail.com>
2017-07-26 19:42:52 -0300

    Added paginator back.

--- groups/views.py
+++ groups/views.py
@@ -15,23 +15,22 @@ def group_list(request):
     if request.user.is_staff or request.user.is_superuser:
         queryset_list = Group.objects.all()
 
-    # paginator = Paginator(queryset_list, 5)  # Show 5 contacts per page
-    # page_request_var = 'page'
-    #
-    # page = request.GET.get(page_request_var)
-    # try:
-    #     queryset = paginator.page(page)
-    # except PageNotAnInteger:
-    #     # If page is not an integer, deliver first page.
-    #     queryset = paginator.page(1)
-    # except EmptyPage:
-    #     # If page is out of range (e.g. 9999), deliver last page of results.
-    #     queryset = paginator.page(paginator.num_pages)
+    paginator = Paginator(queryset_list, 5)  # Show 5 contacts per page
+    page_request_var = 'page'
+
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
     context = {
-        # "object_list": queryset,
-        "object_list": queryset_list,
+        "object_list": queryset,
         "title": "List",
-        # "page_request_var": page_request_var,
+        "page_request_var": page_request_var,
     }
     return render(request, "group_list.html", context)
 
