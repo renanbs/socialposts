df73cc55ba235e92f73d4d2f99a6818c0038a3ee
renan.biegel <renan.biegel@gmail.com>
2017-07-28 17:12:19 -0300

    Added observation to groups and make url unique.

--- groups/models.py
+++ groups/models.py
@@ -5,13 +5,14 @@ from django.core.urlresolvers import reverse
 
 class Group(models.Model):
     title = models.CharField(max_length=200)
-    url = models.URLField()
+    url = models.URLField(unique=True)
     contact_updated = models.DateField(auto_now=False, auto_now_add=True)
     group_status = models.CharField(max_length=20)
     admin = models.CharField(max_length=20)
     admin_status = models.CharField(max_length=20)
     frequency = models.IntegerField()  # allowed post frequency
     frq_scale = models.CharField(max_length=20, blank=True)
+    obs = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
