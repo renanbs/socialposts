6607fd1ce3bfec75ed7e3caf93cab3415ede6dcc
Renan <renan.biegel@gmail.com>
2017-08-26 15:18:50 -0300

    Enabling debug.

--- socialposts/settings/production.py
+++ socialposts/settings/production.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 SECRET_KEY = '#ayvr#)4c1#+t@(8q=a(e47bpol+d5g8h_9pcb8563$73qi4$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'socialpostscontrol.herokuapp.com']
 
