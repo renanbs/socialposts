62379a49e12a23f4d53b87936600d718897a73a7
Renan <renan.biegel@gmail.com>
2017-08-27 16:50:46 -0300

    testing

--- socialposts/settings.py
+++ socialposts/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
