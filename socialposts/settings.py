79290fbf3509e8bd20b8b7e9ee7ca5b2f02ec962
Renan <renan.biegel@gmail.com>
2017-08-27 16:41:56 -0300

    testing

--- socialposts/settings.py
+++ socialposts/settings.py
@@ -130,13 +130,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
 
 STATUS_LIST = ('------', 'ok', 'nok', 'waiting in', 'waiting admin', 'admin ignored')
 STATUS_CHOICES = (
